eca22ef2e3391d7643ca23a4a88ba1d944d5bab7
David Leonard <sephirothcloud1025@yahoo.com>
2014-04-29 11:52:00 -0400

    Removing files.py

--- scripts/files.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import netCDF4
-path = "/cephfs/fhs/data/in/acspo/www.star.nesdis.noaa.gov/pub/sod/sst/micros_data/acspo_nc/npp/2014-07-11/"
-path
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [ f for f in listdir(path) if isfile(join(path, f))]
-onlyfiles

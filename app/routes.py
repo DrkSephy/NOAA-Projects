482a6c120bf49f289900c418554e5d5e5f7de534
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-08 11:06:44 -0400

    Adding post route

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources=r'/*', headers='Content-Type')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/getCoordinates', methods=['GET', 'POST'])
+def getCoordinates(data):
+    if request.method == 'POST':
+        print "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)

0cff163bb553363148daabf61a7021eb7daa6db8
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-07 23:00:51 -0400

    Turning point-and-click-demo into flask app

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-    

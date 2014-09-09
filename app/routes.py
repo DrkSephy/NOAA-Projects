0ba02e1c2f21e89977216dcb4ea58220dddf74a8
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-09 12:52:46 -0400

    Working on requests to api

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask.ext.cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app, resources=r'/*', headers='Content-Type')
@@ -12,6 +13,9 @@ def home():
 def getCoordinates():
     print "Pos x: " + request.args.get('pos_x')
     print "Pos y: " + request.args.get('pos_y')
+    url = requests.get('http://placekitten.com/' + request.args.get('pos_x') + '/' + request.args.get('pos_y'))
+    print url
+
     return "Hello"
     
 

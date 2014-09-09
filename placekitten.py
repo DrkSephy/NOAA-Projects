6fbc513385bf5a06e058534783049f765c8afade
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-09 13:18:32 -0400

    Removing index.html, placekitten.py, point.gif

--- placekitten.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-import requests
-
-url = 'http://placekitten.com/g/200/300'
-response = requests.get(url, stream=True)
-with open('img.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response

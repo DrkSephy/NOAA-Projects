304fb2f54c5de96a7de0f311d54f527117b28f62
David Leonard <dleonar00@citymail.cuny.edu>
2014-09-16 14:22:46 -0400

    Removing small scripts for seperately computing latitude, longitude of nc files

--- scripts/longitude.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-import netCDF4
-ds = netCDF4.Dataset("/cephfs/fhs/data/in/acspo/www.star.nesdis.noaa.gov/pub/sod/sst/micros_data/acspo_nc/npp/2014-07-11/ACSPO_V2.30_NPP_VIIRS_2014-07-11_2210-2220_20140714.093352.nc") 
-nr, nc = ds.variables["longitude"].shape
-long = ds.variables["longitude"]
-long[nr//2, nc//2]

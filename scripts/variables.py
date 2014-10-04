fa2aa6c8a0bfc903f039590634fb097afc41326c
David Leonard <sephirothcloud1025@yahoo.com>
2014-10-04 15:06:00 -0400

    Removing variables.py

--- scripts/variables.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import netCDF4
-import numpy as np
-ds = netCDF4.Dataset('ACSPO_V2.30_NPP_VIIRS_2014-07-11_2210-2220_20140714.093352.nc', 'r+', format='NETCDF4')
-sst_regression = np.array(ds.variables['sst_regression'])
-sst_reynolds = np.array(ds.variables['sst_reynolds'])
-anomaly = sst_regression - sst_reynolds
-anomaly.shape

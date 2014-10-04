abcec996befed9138fed81d3c993b64701b53942
David Leonard <sephirothcloud1025@yahoo.com>
2014-10-04 15:05:33 -0400

    Fixing typo within granules.py

--- scripts/granules.py
+++ scripts/granules.py
@@ -6,7 +6,7 @@ from os.path import isfile, join
 from os import listdir
 
 path = "/cephfs/fhs/data/in/acspo/www.star.nesdis.noaa.gov/pub/sod/sst/micros_data/acspo_nc/npp/2014-07-11/"
-onlyfiles = f for f in listdir(path) if isfile(join(path, f))]
+onlyfiles = [ f for f in listdir(path) if isfile(join(path, f))]
 
 i = 1
 for f in onlyfiles:

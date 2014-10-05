ba69f85ceb75b39e3d573db9c53320537e36da6a
David Leonard <sephirothcloud1025@yahoo.com>
2014-10-04 21:48:01 -0400

    Updating anomaly image generator

--- scripts/anomaly_generator.py
+++ scripts/anomaly_generator.py
@@ -9,8 +9,9 @@ orig_path = "/home/DrkSephy/tmp/nc/"
 orig_files = [ f for f in listdir(orig_path) if isfile(join(orig_path, f)) ]
 
 i = 1
-for f in onlyfiles:
-    command = "cwrender --enhance sst_regression --palette Blue-Red --size 1508 --range -4/4" + " " + path + f + " " +  str(i) + ".png"
+for f in orig_files:
+    print "Processing [" + str(i) + "]" + " "  + orig_path + f 
+    command = "cwrender --enhance sst_regression --palette Blue-Red --size 1508 --range -4/4" + " " + orig_path + f + " " +  str(i) + ".png"
     os.system(command)
     i += 1
 

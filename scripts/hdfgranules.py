3c674987fb8c43c94ab1ea8c335e3b5382e4ac6d
David Leonard <sephirothcloud1025@yahoo.com>
2014-10-29 22:24:08 -0400

    Finishing script to computing longitudes and latitudes for Terra granules

--- scripts/hdfgranules.py
+++ scripts/hdfgranules.py
@@ -2,7 +2,7 @@ from os import listdir
 from os.path import isfile, join
 from pyhdf.SD import SD, SDC
 
-path = "/home/DrkSephy/terra"
+path = "/home/DrkSephy/terra/"
 onlyfiles = [ f for f in listdir(path) if isfile(join(path, f)) ]
 computedData = [ ]
 
@@ -10,16 +10,15 @@ computedData = [ ]
 for f in onlyfiles:
     dataset = SD(path + f, SDC.READ)
     # Get longitude
-    lo = f.select('Longitude')
+    lo = dataset.select('Longitude')
     nr, nc = lo[:,:].shape
     long = lo[:,:]
     longitude = long[nr//2, nc//2]
-
     # Get latitude
-    la = f.select('Latitude')
+    la = dataset.select('Latitude')
     numr, numc = la[:,:].shape
     lat = la[:,:]
     latitude = lat[numr//2, numc//2]
-    computedData.append({'Latitude': latitude, 'Longitude': longitude})
+    computedData.append({'Latitude': latitude, 'Longitude': longitude, 'Satellite': 'Terra'})
 
 print computedData

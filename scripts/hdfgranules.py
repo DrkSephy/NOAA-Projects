75be6440d6454abd22834d5cf10434a0afdc5165
David Leonard <sephirothcloud1025@yahoo.com>
2014-10-29 21:50:59 -0400

    Script to compute longitude and latitude

--- scripts/hdfgranules.py
+++ scripts/hdfgranules.py
@@ -4,10 +4,15 @@ f = SD('MOD021KM.A2014295.2340.005.2014296093338.hdf', SDC.READ)
 # print all datasets
 f.datasets()
 # Get a handle to specific datasets
-ff = f.select('Longitude')
+lo = f.select('Longitude')
 # print out datasets
-ff[:,:]
+lo[:,:]
 # Get number of rows/columns
-nr, nc = ff[:,:].shape
-long = ff[:,:]
+nr, nc = lo[:,:].shape
+long = lo[:,:]
 longitude = long[nr//2, nc//2]
+
+la = f.select('Latitude')
+numr, numc = la[:,:].shape
+lat = la[:,:]
+latitude = lat[numr//2, numc//2]

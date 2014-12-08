71eb28c22cc3edd617c6d77abeebdfe835ddfffc
David Leonard <sephirothcloud1025@yahoo.com>
2014-12-08 16:44:50 -0500

    idl/findfiles.py: Implementing getSatellite function

--- idl/findfiles.py
+++ idl/findfiles.py
@@ -6,6 +6,20 @@
 #     findfiles.ksh
 #------------------------
 
-satellite = ''
-inputdir  = ''
-mmdd      = ''
+
+def getSatellite(satellite):
+    # Sets input_dir based on satellite
+    input_dir = ''
+    if satellite == 'NOAA16_GHRR':
+        input_dir = gpnl
+    elif satellite == 'NOAA17_GHRR':
+        input_dir = gpnm
+    elif satellite == 'NOAA18_GHRR':
+        input_dir = gpnn
+    elif satellite == 'NOAA19_GHRR':
+        input_dir = gpnp 
+    elif satellite == 'METOPA_GHRR':
+        input_dir = gpm2 
+    elif satellite == 'METOPA_FRAC':
+        input_dir = frac
+    return input_dir

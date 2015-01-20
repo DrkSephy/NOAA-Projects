3dc6e76882d79bd23a591190f059a7d5cf5217f5
David Leonard <sephirothcloud1025@yahoo.com>
2015-01-19 19:24:19 -0500

    processor.py: Updating processor script

--- idl/processor.py
+++ idl/processor.py
@@ -8,8 +8,9 @@ from os import listdir
 from os.path import isfile, join
 from subprocess import Popen, PIPE
 
+file = 
 # Read a granule in
-ds = SD('regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf', SDC.READ)
+ds = SD(file, SDC.READ)
 # Grab l2p_flags dataset
 flags = ds.select('l2p_flags')
 flags = np.array(flags[:,:], dtype='uint16')
@@ -25,19 +26,19 @@ bit16_mask = np.bitwise_and(flags, bit16)>0
 
 
 # Command 1: sea_surface_temperature
-sst = 'cwmath --verbose --template sea_surface_temperature --expr ' + ' ' + '''"sea_surface_temperature = sea_surface_temperature"''' + ' ' + 'regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf' + ' ' + 'math.hdf'
+sst = 'cwmath --verbose --template sea_surface_temperature --expr ' + ' ' + '''"sea_surface_temperature = sea_surface_temperature"''' + ' ' + file + ' ' + 'math.hdf'
 
 # Command 2: dt_analysis
-dt_analysis = 'cwmath --verbose --template dt_analysis --expr' + ' ' + '''"dt_analysis = sea_surface_temperature"''' + ' ' + 'regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf' + ' ' + 'math.hdf'
+dt_analysis = 'cwmath --verbose --template dt_analysis --expr' + ' ' + '''"dt_analysis = sea_surface_temperature"''' + ' ' + file + ' ' + 'math.hdf'
 
 # Command 3: Create missing flag
-missing_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' +  '''"missing_flag = select ( and (l2p_flags, 49152) == 49152, 1., 0. )"''' +  ' ' + 'regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf' + ' ' + 'math.hdf'
+missing_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' +  '''"missing_flag = select ( and (l2p_flags, 49152) == 49152, 1., 0. )"''' +  ' ' + file + ' ' + 'math.hdf'
 
 # Command 4: Create cloud flag
-cloud_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' +  '''"cloud_flag = select (and (l2p_flags, 49152) < 16384, 0., 1.)"''' + ' ' + 'regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf' + ' ' + 'math.hdf'
+cloud_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' +  '''"cloud_flag = select (and (l2p_flags, 49152) < 16384, 0., 1.)"''' + ' ' + file + ' ' + 'math.hdf'
 
 # Command 5: Create glint flag
-glint_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' + '''"glint_flag = select ( (and (l2p_flags, 8192) == 8192), 1., 0.)"''' + ' ' + 'regdayBS20141122085952-STAR-L2P_GHRSST-SST1m-GHRR_METOPA-v02.0-fv01.0.hdf' + ' ' + 'math.hdf'
+glint_flag = 'cwmath --verbose --template sea_surface_temperature --expr' + ' ' + '''"glint_flag = select ( (and (l2p_flags, 8192) == 8192), 1., 0.)"''' + ' ' + file + ' ' + 'math.hdf'
 
 
 cmds = []
